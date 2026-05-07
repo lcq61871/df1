a960beeac65a3ea6b4f58e162db651f26858bd68
lcq61871 <95287580+lcq61871@users.noreply.github.com>
2026-05-08 00:21:36 +0800

    更新 sx.py

--- sx.py
+++ sx.py
@@ -13,8 +13,8 @@ target_channels = [
     '1CCTV5[1920*1080]', '1CCTV8[1920*1080]', '凤凰中文',
     '凤凰资讯', '凤凰香港', '111澳視澳門',
 	'凤凰中文', '1湖南都市高清 12M', '1娱乐新闻',
-    '1亚洲武侠', 'CCTV-4HD', 'CCTV-5HD',
-    'CCTV-8HD', 'CCTV-12HD', '1CCTV-8 电视剧'
+    '1亚洲武侠', 'CCTV-4中文国际', 'CCTV-5体育',
+    'CCTV-8电视剧', 'CCTV-12社会与法', '1CCTV-8 电视剧'
 ]
 
 exclude_keywords = ['chinamobile', 'tvgslb', '购物', '理财']
@@ -44,10 +44,10 @@ except FileNotFoundError:
     print("⚠️ 本地文件未找到")
 
 remote_urls = [
-	'1https://raw.githubusercontent.com/pxiptv/live/bd781789b21bb00f650183bfb374422a1016785e/live.txt',
+	'https://raw.githubusercontent.com/q1017673817/iptvz/main/组播_广东电信.txt',
     '1https://raw.githubusercontent.com/luoye20230624/hndxzb/refs/heads/main/iptv_list.txt',
     '1https://raw.githubusercontent.com/ngdikman/hksar/refs/heads/main/dianxin.txt',
-	'https://raw.githubusercontent.com/JFApril/D/refs/heads/main/TestLive.txt',
+	'1https://raw.githubusercontent.com/JFApril/D/refs/heads/main/TestLive.txt',
     '1https://raw.githubusercontent.com/lcq61871/iptvz/refs/heads/main/maotv.txt'
 ]
 
